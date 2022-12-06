4bead928cd741d585aa88979d4be4d730dc98eab
Pranit Dakhinkar <97027846+dakhinkar@users.noreply.github.com>
2022-12-07 00:00:08 +0530

    Update script.js
    script

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+const sharableUrl = document.getElementById('sharableUrl');
+const name = document.getElementById('name');
+const year = document.getElementById('year');
+
+const submitHandler = () => {
+    // e.preventDefault();  
+    const nameData = `name=${name.value}`;
+    const yearData = `year=${year.value}`;
+    let ans ='?'
+    if (name.value.trim() !== "") {
+        ans = ans + nameData;
+    }
+    if (name.value.trim() !== "" && year.value > 0) {
+        ans = ans + "&" + yearData;
+    }
+    if (name.value.trim() === "" && year.value >0) {
+        ans = ans + yearData;
+    }
+    const text = sharableUrl.innerText;
+    sharableUrl.innerText = text + ans;
+   
+}
